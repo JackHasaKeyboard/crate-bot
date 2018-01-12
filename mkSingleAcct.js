2d571c0ce51e67f5db9dbca1e374694bb4eda9ea
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-01-12 14:11:34 -0700

    Refactor

--- mkSingleAcct.js
+++ mkSingleAcct.js
@@ -1,33 +1,25 @@
-const SteamUser = require("steam-user");
-const prompt = require("prompt");
-const _ = require("lodash");
-const cred = require("./cred.js");
-
-var User = new SteamUser();
+const SteamUser = require('steam-user');
+const prompt = require('prompt');
+const cred = require('./cred.js');
 
 
-// log on, must be logged in as existing user
+// log in, must be logged in as existing user
+const User = new SteamUser();
+
 User.logOn(cred.jack);
 
-User.on("loggedOn", function(details) {
-	console.log("Successfully logged in");
+User.on('loggedOn', function() {
+	console.log('Successfully logged in');
 
 	prompt.start();
 
-	prompt.get(["no"], function(err, no) {
+	prompt.get('no', function(err, i) {
 		/* create */
-		User.createAccount("projectcrate" + no.no, cred.crate.password + no.no, "steamprojectcrate@gmail.com", function(result, steamid) {
+		User.createAccount('projectcrate' + i.no, cred.crate.password + i.no, 'steamprojectcrate@gmail.com', function(result, steamid) {
 			if (result == 1) { // ok
-				console.log("Successfully created projectcrate" + no.no);
-
-				process.exit();
-
-				// edit details
-				// User.logOn({userName: "projectcrate" + no, password: cred.crate.password + no});
-
-				// User.setPersona(1, "Project Crate (" + no + ")");
+				console.log('Successfully created projectcrate' + i.no);
 			} else {
-				console.log("Failed to create account; error " + result);
+				console.log('Failed to create account; error ' + result);
 			}
 		});
 	});
