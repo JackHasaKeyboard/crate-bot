0f219abc97daea511c32c71b2322ceec931e85e2
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-01-25 14:07:16 -0700

    Add error handling for Steam Guard and CAPTCHA

--- storage.js
+++ storage.js
@@ -8,56 +8,111 @@ const cred = require('./cred.js');
 updateDetail = function(no, callback) {
 	var community = new SteamCommunity;
 
+	update = function() {
+		community.editProfile({
+			'name': 'Project Crate (' + no + ')',
+			'summary': `
+			Storage account #` + no + ` for Project Crate, a project put together to amass as many Crates as possible and break a world record.
+
+			We'd love it if you'd donate some Crates! To make a donation, send the dude in charge (JackThaGamer) a trade and you'll be marked down for your donation.
+
+			Dude in charge: http://steamcommunity.com/id/JackThaGamer/
+			Steam Group: http://steamcommunity.com/groups/ProjectCrate
+
+			The profile picture was sketched by Pobito: http://steamcommunity.com/id/Pobbimann
+			And colored by Shiny: http://steamcommunity.com/profiles/76561198066874043
+			`,
+			'customURL': 'projectcrate' + no,
+
+			// null settings
+			'realName': '',
+			'country': '',
+			'state': '',
+			'city': '',
+			'background': '',
+			'featuredBadge': '',
+			'primaryGroup': '103582791435442783' // Project Crate
+		}, function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+
+		community.profileSettings({
+			'profile': 'Public',
+			'comments': 1, // private
+			'inventory': 'Public',
+			'inventoryGifts': true // public
+		}, function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+
+		community.joinGroup('103582791435442783' /* Project Crate */, function(err, result) {
+			if (err) {
+				console.log(err);
+			}
+		});
+
+		community.uploadAvatar('crate.png', null, function(err, result) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	}
+
 	community.login({
 		'accountName': 'projectcrate' + no,
 		'password': cred.crate.password + no
 	}, function(err, user) {
 		if (err) {
 			console.log(err);
+
+			switch(err.message) {
+				case 'SteamGuard':
+					prompt.get(['steamGuard'], function(err, i) {
+						community.login({
+							'accountName': 'projectcrate' + no,
+							'password': cred.crate.password + no,
+							'authCode': i.steamGuard
+						}, function(err, user) {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log('Successfully logged in');
+
+								update();
+							}
+						});
+					});
+
+					break;
+
+				case 'CAPTCHA':
+					prompt.get(['captcha'], function(err, i) {
+						community.login({
+							'accountName': 'projectcrate' + no,
+							'password': cred.crate.password + no,
+							'captcha': i.captcha
+						}, function(err, user) {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log('Successfully logged in');
+
+								update();
+							}
+						});
+					});
+
+					break;
+			}
 		}
 
 		community.loggedIn(function(err, loggedIn) {
 			if (loggedIn) {
-				community.editProfile({
-					'name': 'Project Crate (' + no + ')',
-					'summary': `
-					Storage account #` + no + ` for Project Crate, a project put together to amass as many Crates as possible and break a world record.
-
-					We'd love it if you'd donate some Crates! To make a donation, send the dude in charge (JackThaGamer) a trade and you'll be marked down for your donation.
-
-					Dude in charge: http://steamcommunity.com/id/JackThaGamer/
-					Steam Group: http://steamcommunity.com/groups/ProjectCrate
-
-					The profile picture was sketched by Pobito: http://steamcommunity.com/id/Pobbimann
-					And colored by Shiny: http://steamcommunity.com/profiles/76561198066874043
-					`,
-					'customURL': 'projectcrate' + no,
-
-					// null settings
-					'realName': '',
-					'country': '',
-					'state': '',
-					'city': '',
-					'background': '',
-					'featuredBadge': '',
-					'primaryGroup': '103582791435442783' // Project Crate
-				}, function(err) {
-					if (err) {
-						console.log(err);
-					}
-				});
-
-				community.joinGroup('103582791435442783' /* Project Crate */, function(err, result) {
-					if (err) {
-						console.log(err);
-					}
-				});
-
-				community.uploadAvatar('crate.png', null, function(err, result) {
-					if (err) {
-						console.log(err);
-					}
-				});
+				update();
 			}
 		});
 	});
