7cb922b0259ad70b83f1bd8855bc484a5c4d1adc
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-04-02 04:12:15 -0600

    Refactor

--- general.js
+++ general.js
@@ -1,96 +1,90 @@
-const SteamUser = require('steam-user');
-const fetch = require('node-fetch');
-const prompt = require('prompt');
-const _ = require('lodash');
-const cred = require('./cred.js');
-
-initAcct = function(no, callback) {
-	// log in, must be logged in as existing user
-	var user = new SteamUser();
-
-	user.logOn(cred.jack);
-
-	user.on('loggedOn', function() {
-		console.log('Successfully logged in');
-
-		/* create */
-		user.createAccount('projectcrate' + no, cred.crate.password + no, 'steamprojectcrate@gmail.com', function(result, steamid) {
-			if (result == 1 /* ok */) {
-				callback();
-			} else {
-				console.log('Failed to create account; error ' + result);
-			}
-		});
-	});
-}
-
-chkStatus = function(no) {
-	// resolve vanity URL into an ID, if missing then the user either needs to be created or needs to be updated
-	var url = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + cred.key + '&vanityurl=projectcrate' + no;
-
-	return fetch(url).then(function(response) {
-		return response.json();
-	}).then(function(json) {
-		if (json.response.success == 1 /* matched */) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-}
-
-updatePassword = function(no, current) {
-	var user = new SteamUser();
-
-	user.logOn({
-		'accountName': 'projectcrate' + no,
-		'password': current
-	});
-
-	user.on('loggedOn', function() {
-		console.log('Successfully logged in');
-
-		user.requestPasswordChangeEmail(current, function() {
-			prompt.get(['code'], function(err, i) {
-				user.changePassword(current, cred.crate.password + no, i.code, function(err) {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log('Successfully updated password');
-					}
-				});
-			});
-		});
-	});
-}
-
-getId = function(vanity) {
-	return fetch('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + cred.key +'&vanityurl=' + vanity).then(function(response) {
-		return response.json();
-	}).then(function(json) {
-		if (json.response.success == 1) {
-			var id = json.response.steamid;
-
-			return id;
-		}
-	});
-}
-
-iter = function(callback) {
-	prompt.get(['floor', 'roof'], function(err, i) {
-		_.range(i.floor, i.roof).forEach(function(no, idx) {
-			setTimeout(function() {
-				callback();
-			}, idx * 1000);
-		});
-	});
-}
+const SteamUser = require('steam-user'),
+	fetch = require('node-fetch'),
+	prompt = require('prompt'),
+	_ = require('lodash'),
+	cred = require('./cred.js')
 
 
 module.exports = {
-	initAcct,
-	chkStatus,
-	updatePassword,
-	getId,
-	iter
+	initAcct: function(no, callback) {
+		// log in, must be logged in as existing user
+		var user = new SteamUser();
+
+		user.logOn(cred.jack);
+
+		user.on('loggedOn', function() {
+			console.log('Successfully logged in');
+
+			/* create */
+			user.createAccount('projectcrate' + no, cred.crate.password + no, 'steamprojectcrate@gmail.com', function(result, steamid) {
+				if (result == 1 /* ok */) {
+					callback();
+				} else {
+					console.log('Failed to create account; error ' + result);
+				}
+			});
+		});
+	},
+
+	chkStatus: function(no) {
+		// resolve vanity URL into an ID, if missing then the user either needs to be created or needs to be updated
+		var url = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + cred.key + '&vanityurl=projectcrate' + no;
+
+		return fetch(url).then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			if (json.response.success == 1 /* matched */) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	},
+
+	updatePassword: function(no, current) {
+		var user = new SteamUser();
+
+		user.logOn({
+			'accountName': 'projectcrate' + no,
+			'password': current
+		});
+
+		user.on('loggedOn', function() {
+			console.log('Successfully logged in');
+
+			user.requestPasswordChangeEmail(current, function() {
+				prompt.get(['code'], function(err, i) {
+					user.changePassword(current, cred.crate.password + no, i.code, function(err) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log('Successfully updated password');
+						}
+					});
+				});
+			});
+		});
+	},
+
+	getId: function(vanity) {
+		return fetch('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + cred.key +'&vanityurl=' + vanity).then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			if (json.response.success == 1) {
+				var id = json.response.steamid;
+
+				return id;
+			}
+		});
+	},
+
+	iter: function(callback) {
+		prompt.get(['floor', 'roof'], function(err, i) {
+			_.range(i.floor, i.roof).forEach(function(no, idx) {
+				setTimeout(function() {
+					callback();
+				}, idx * 1000);
+			});
+		});
+	}
 }
--- storage.js
+++ storage.js
@@ -1,175 +1,40 @@
-const SteamUser = require('steam-user');
-const SteamCommunity = require('steamcommunity');
-const SteamTradeOfferManager = require('steam-tradeoffer-manager');
-const prompt = require('prompt');
-const fetch = require('node-fetch');
-const general = require('./general.js');
-const cred = require('./cred.js');
+const SteamUser = require('steam-user'),
+	SteamCommunity = require('steamcommunity'),
+	SteamTradeOfferManager = require('steam-tradeoffer-manager'),
+	prompt = require('prompt'),
+	fetch = require('node-fetch'),
+	general = require('./general.js'),
+	cred = require('./cred.js')
 
 
-updateDetail = function(no) {
-	var community = new SteamCommunity;
+module.exports = {
+	updateDetail: function(no) {
+		var community = new SteamCommunity;
 
-	update = async function() {
-		await community.editProfile({
-			'name': 'Project Crate (' + no + ')',
-			'customURL': 'projectcrate' + no,
+		update = async function() {
+			await community.editProfile({
+				'name': 'Project Crate (' + no + ')',
+				'customURL': 'projectcrate' + no,
 
-			// null settings
-			'realName': '',
-			'country': '',
-			'state': '',
-			'city': '',
-			'background': '',
-			'featuredBadge': '',
-			'primaryGroup': '103582791435442783' // Project Crate
-		}, function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-
-		general.getId('projectcrate' + no).then(function(id) {
-			community.getUserInventoryContents(id, 440, 2, true, function(err, inv) {
+				// null settings
+				'realName': '',
+				'country': '',
+				'state': '',
+				'city': '',
+				'background': '',
+				'featuredBadge': '',
+				'primaryGroup': '103582791435442783' // Project Crate
+			}, function(err) {
 				if (err) {
 					console.log(err);
-				} else {
-					var stock = {};
-
-					inv.forEach(function(item) {
-						if (!stock[item.name]) {
-							stock[item.name] = 0;
-						}
-
-						stock[item.name]++;
-					});
-
-					community.editProfile({
-						'summary': `
-						Storage account #` + no + ` for Project Crate, a project put together to amass as many Crates as possible and break a world record.
-
-						We'd love it if you'd donate some Crates! To make a donation, send the dude in charge (JackThaGamer) a trade and you'll be marked down for your donation.
-
-						Dude in charge: http://steamcommunity.com/id/JackThaGamer/
-						Steam Group: http://steamcommunity.com/groups/ProjectCrate
-
-						The profile picture was sketched by Pobito: http://steamcommunity.com/id/Pobbimann
-						And colored by Shiny: http://steamcommunity.com/profiles/76561198066874043
-
-						=== Bot info ===
-						` + JSON.stringify(stock, null, '\t') + `
-						`,
-					}, function(err) {
-						if (err) {
-							console.log(err);
-						}
-					});
 				}
 			});
-		});
 
-		community.profileSettings({
-			'profile': 'Public',
-			'comments': 1, // private
-			'inventory': 'Public',
-			'inventoryGifts': true // public
-		}, function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-
-		community.joinGroup('103582791435442783' /* Project Crate */, function(err, result) {
-			if (err) {
-				console.log(err);
-			}
-		});
-
-		community.uploadAvatar('crate.png', null, function(err, result) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
-
-	community.login({
-		'accountName': 'projectcrate' + no,
-		'password': cred.crate.password + no
-	}, function(err, user) {
-		if (err) {
-			console.log(err);
-
-			switch(err.message) {
-				case 'SteamGuard':
-					prompt.get(['steamGuard'], function(err, i) {
-						community.login({
-							'accountName': 'projectcrate' + no,
-							'password': cred.crate.password + no,
-							'authCode': i.steamGuard
-						}, function(err, user) {
-							if (err) {
-								console.log(err);
-							} else {
-								console.log('Successfully logged in');
-
-								update();
-							}
-						});
-					});
-
-					break;
-
-				case 'CAPTCHA':
-					prompt.get(['captcha'], function(err, i) {
-						community.login({
-							'accountName': 'projectcrate' + no,
-							'password': cred.crate.password + no,
-							'captcha': i.captcha
-						}, function(err, user) {
-							if (err) {
-								console.log(err);
-							} else {
-								console.log('Successfully logged in');
-
-								update();
-							}
-						});
-					});
-
-					break;
-			}
-		}
-
-		community.loggedIn(function(err, loggedIn) {
-			if (loggedIn) {
-				update();
-			}
-		});
-	});
-}
-
-mkAcct = function(no) {
-	general.initAcct(no, function() {
-		updateDetail(no, function() {
-			console.log('Successfully created projectcrate' + no);
-		});
-	});
-}
-
-log = function(no) {
-	const community = new SteamCommunity;
-
-	general.getId('projectcrate' + no).then(function(id) {
-		community.getUserInventoryContents(id, 440, 2, true, function(err, inv) {
-			if (err) {
-				console.log(err);
-			} else {
-				fs.readFile('status.json', function(err, data) {
+			general.getId('projectcrate' + no).then(function(id) {
+				community.getUserInventoryContents(id, 440, 2, true, function(err, inv) {
 					if (err) {
 						console.log(err);
 					} else {
-						var obj = JSON.parse(data);
-
 						var stock = {};
 
 						inv.forEach(function(item) {
@@ -180,27 +45,157 @@ log = function(no) {
 							stock[item.name]++;
 						});
 
-						if (!obj[no]) {
-							obj[no] = {};
-						}
+						community.editProfile({
+							'summary': `
+							Storage account #` + no + ` for Project Crate, a project put together to amass as many Crates as possible and break a world record.
 
-						obj[no] = stock;
+							We'd love it if you'd donate some Crates! To make a donation, send the dude in charge (JackThaGamer) a trade and you'll be marked down for your donation.
 
-						fs.writeFile('status.json', JSON.stringify(obj, null, 2 /* format to be readable */), function(err) {
+							Dude in charge: http://steamcommunity.com/id/JackThaGamer/
+							Steam Group: http://steamcommunity.com/groups/ProjectCrate
+
+							The profile picture was sketched by Pobito: http://steamcommunity.com/id/Pobbimann
+							And colored by Shiny: http://steamcommunity.com/profiles/76561198066874043
+
+							=== Bot info ===
+							` + JSON.stringify(stock, null, '\t') + `
+							`,
+						}, function(err) {
 							if (err) {
 								console.log(err);
 							}
 						});
 					}
 				});
+			});
+
+			community.profileSettings({
+				'profile': 'Public',
+				'comments': 1, // private
+				'inventory': 'Public',
+				'inventoryGifts': true // public
+			}, function(err) {
+				if (err) {
+					console.log(err);
+				}
+			});
+
+			community.joinGroup('103582791435442783' /* Project Crate */, function(err, result) {
+				if (err) {
+					console.log(err);
+				}
+			});
+
+			community.uploadAvatar('crate.png', null, function(err, result) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		}
+
+		community.login({
+			'accountName': 'projectcrate' + no,
+			'password': cred.crate.password + no
+		}, function(err, user) {
+			if (err) {
+				console.log(err);
+
+				switch(err.message) {
+					case 'SteamGuard':
+						prompt.get(['steamGuard'], function(err, i) {
+							community.login({
+								'accountName': 'projectcrate' + no,
+								'password': cred.crate.password + no,
+								'authCode': i.steamGuard
+							}, function(err, user) {
+								if (err) {
+									console.log(err);
+								} else {
+									console.log('Successfully logged in');
+
+									update();
+								}
+							});
+						});
+
+						break;
+
+					case 'CAPTCHA':
+						prompt.get(['captcha'], function(err, i) {
+							community.login({
+								'accountName': 'projectcrate' + no,
+								'password': cred.crate.password + no,
+								'captcha': i.captcha
+							}, function(err, user) {
+								if (err) {
+									console.log(err);
+								} else {
+									console.log('Successfully logged in');
+
+									update();
+								}
+							});
+						});
+
+						break;
+				}
 			}
+
+			community.loggedIn(function(err, loggedIn) {
+				if (loggedIn) {
+					update();
+				}
+			});
 		});
-	});
-}
+	},
 
+	mkAcct: function(no) {
+		general.initAcct(no, function() {
+			updateDetail(no, function() {
+				console.log('Successfully created projectcrate' + no);
+			});
+		});
+	},
 
-module.exports = {
-	updateDetail,
-	mkAcct,
-	log
+	log: function(no) {
+		const community = new SteamCommunity;
+
+		general.getId('projectcrate' + no).then(function(id) {
+			community.getUserInventoryContents(id, 440, 2, true, function(err, inv) {
+				if (err) {
+					console.log(err);
+				} else {
+					fs.readFile('status.json', function(err, data) {
+						if (err) {
+							console.log(err);
+						} else {
+							var obj = JSON.parse(data);
+
+							var stock = {};
+
+							inv.forEach(function(item) {
+								if (!stock[item.name]) {
+									stock[item.name] = 0;
+								}
+
+								stock[item.name]++;
+							});
+
+							if (!obj[no]) {
+								obj[no] = {};
+							}
+
+							obj[no] = stock;
+
+							fs.writeFile('status.json', JSON.stringify(obj, null, 2 /* format to be readable */), function(err) {
+								if (err) {
+									console.log(err);
+								}
+							});
+						}
+					});
+				}
+			});
+		});
+	}
 }
